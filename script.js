0871776d97a88085676e8311760c980a2d6ee497
Khushi-Singh-04 <khushi.singh2913@gmail.com>
2025-08-26 23:41:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const navLinks = document.querySelectorAll('.nav a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.forEach(item => item.classList.remove('active'));
+
+    link.classList.add('active');
+  });
+});
+
+
